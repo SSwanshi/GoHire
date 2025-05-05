bc3135c5bd9f27246a79fb7ea83509f8c6f01b25
Sarvjeet Swanshi <adityaspgm@gmail.com>
2025-05-05 23:00:11 +0530

    yehhhhhhh

--- applicant-server/config/recruiterDB.js
+++ applicant-server/config/recruiterDB.js
@@ -1,34 +1,34 @@
-// // config/recruiterDB.js
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-
-// let recruiterConn;
-
-// const connectRecruiterDB = async () => {
-//     if (!recruiterConn) {
-//         recruiterConn = await mongoose.createConnection(process.env.MONGO_URI_RECRUITERS, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             serverSelectionTimeoutMS: 20000,
-//         });
-//         console.log("✅ Connected to recruiter DB from applicants server");
-//     }
-//     return recruiterConn;
-// };
-
-// module.exports = connectRecruiterDB;
-
+// config/recruiterDB.js
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const recruiterDB = mongoose.createConnection(process.env.RECRUITER_DB_URI || 'mongodb+srv://gohire:gohire12345678@gohire.kzwudx0.mongodb.net/golfire_recruiters?retryWrites=true&w=majority');
+let recruiterConn;
 
-recruiterDB.on('connected', () => {
-  console.log('✅ Recruiter DB Connected');
-});
+const connectRecruiterDB = async () => {
+    if (!recruiterConn) {
+        recruiterConn = await mongoose.createConnection(process.env.MONGO_URI_RECRUITERS, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 20000,
+        });
+        console.log("✅ Connected to recruiter DB from applicants server");
+    }
+    return recruiterConn;
+};
 
-recruiterDB.on('error', (err) => {
-  console.error('❌ Recruiter DB Connection Error', err);
-});
+module.exports = connectRecruiterDB;
 
-module.exports = recruiterDB;
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const recruiterDB = mongoose.createConnection(process.env.RECRUITER_DB_URI || 'mongodb+srv://gohire:gohire12345678@gohire.kzwudx0.mongodb.net/golfire_recruiters?retryWrites=true&w=majority');
+
+// recruiterDB.on('connected', () => {
+//   console.log('✅ Recruiter DB Connected');
+// });
+
+// recruiterDB.on('error', (err) => {
+//   console.error('❌ Recruiter DB Connection Error', err);
+// });
+
+// module.exports = recruiterDB;
