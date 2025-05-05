c19aba6f8768e79bceccbb5d09939030a324e59e
kartik <raghu1ckartik.rk@gmail.com>
2025-05-05 14:10:26 +0530

    added expiry date

--- applicant-server/models/recruiter/Internships.js
+++ applicant-server/models/recruiter/Internships.js
@@ -10,7 +10,12 @@ const internshipSchema = new mongoose.Schema({
     intExperience: Number,
     intPositions: Number,
     intCompany: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
-    createdBy: mongoose.Schema.Types.ObjectId
+    createdBy: mongoose.Schema.Types.ObjectId,
+    intExpiry: { 
+        type: Date, 
+        required: true,
+        default: () => new Date(Date.now() + 30*24*60*60*1000) 
+    }
 }, { timestamps: true });
 
 module.exports = (connection) => {
--- applicant-server/models/recruiter/Job.js
+++ applicant-server/models/recruiter/Job.js
@@ -11,7 +11,12 @@ const jobSchema = new mongoose.Schema({
     jobExperience: Number,
     noofPositions: Number,
     jobCompany: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
-    createdBy: mongoose.Schema.Types.ObjectId
+    createdBy: mongoose.Schema.Types.ObjectId,
+    jobExpiry: { 
+        type: Date, 
+        required: true,
+        default: () => new Date(Date.now() + 30*24*60*60*1000) 
+    }
 }, { timestamps: true });
 
 module.exports = (connection) => {return connection.models.Job || connection.model('Job', jobSchema)};
