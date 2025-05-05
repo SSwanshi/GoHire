6c48d88352f78b5ec2684d45cf419f9388983a8f
Sarvjeet Swanshi <147067870+SSwanshi@users.noreply.github.com>
2025-05-06 04:12:53 +0530

    Update Recruiter.js

--- admin-server/models/Recruiter.js
+++ admin-server/models/Recruiter.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const recruiterSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  company: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Recruiter', recruiterSchema);
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    createdAt: { type: Date, default: Date.now }
+  },{ timestamps: true });
+  
+  module.exports = recruiterSchema;
